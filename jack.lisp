240854c0d20a15dca40ef270a46ddda77ce3ae1e
0xsuk <iam0xsuk@gmail.com>
2023-10-04 12:46:25 +0900

    opening client

--- jack.lisp
+++ jack.lisp
@@ -1,15 +1,32 @@
 (defpackage :jack
   (:use
    :cl
-   :cffi))
+   :cffi)
+  (:export
+   :nframes_t
+   :port_t
+   :jack-set-process-callback
+   :jack-client-open
+   ))
 
 (in-package :jack)
 
 (defctype size_t :unsigned-int)
-(defctype jack_nframes_t :uint32)
-(defctype jack_port_t :pointer)
+(defctype nframes_t :uint32)
+(defctype port_t :pointer)
+
+(define-foreign-library libjack
+  (:unix (:or "libjack.so" "libjack.so.0"))
+  (t (:default "libjack")))
+
+(use-foreign-library libjack)
 
 (defcfun "jack_set_process_callback" :int
   (client :pointer)
   (process_callback :pointer)
   (arg :int))
+
+(defcfun "jack_client_open" :pointer
+  (client-name :string)
+  (options :int)
+  (status :pointer))
--- jockey.lisp
+++ jockey.lisp
@@ -3,3 +3,10 @@
    :cl))
 
 (in-package :jockey)
+
+(cffi:defcallback process-callback :int ((nframes jack:nframes_t) (arg :pointer))
+  (format t "process_callback")
+  0)
+
+(let ((client (jack:jack-client-open "my-client" 0 (cffi:null-pointer))))
+  (jack:jack-set-process-callback client (cffi:callback process-callback) (cffi:null-pointer)))
